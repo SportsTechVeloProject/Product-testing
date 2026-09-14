b03ed9edd9c24ed3c3332e9acbba4c69ef2f1516
Arvid <arvidaxe@gmail.com>
2026-09-15 00:14:49 +0200

    add magdwick data to video sync

--- Python/videosync.py
+++ Python/videosync.py
@@ -7,29 +7,52 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import pandas as pd
 
-# Load the adjusteds IMU data
-resampled_df = pd.read_csv('Python/adjusted_resampled_imu_data_30Hz.csv')
-imu_times = resampled_df['t'].values
-imu_y = resampled_df['y'].values
-
+# Parameters: Note that the imu data and video should have the same framerate
+imudata_raw = pd.read_csv('Python/adjusted_resampled_imu_data_30Hz.csv')
+magdwick = pd.read_csv('Python/madgwick_right_data_30Hz.csv')
 video_path = 'DataSets/DataSet_100926/video1.mp4'  # Made for attempt 1
+
+
+FIRST_SQUAT_IMU_TIME = 5.9
+FIRST_SQUAT_VIDEO_TIME = 5.4
+VIDEO_IMU_DIFF = FIRST_SQUAT_IMU_TIME - FIRST_SQUAT_VIDEO_TIME
+
+# Load the adjusted IMU data
+imu_times = imudata_raw['t'].values
+imu_y = imudata_raw['y'].values
+
+magdwick_times = magdwick['t'].values
+magdwick_acc = magdwick['acc'].values
+
+# Verify the SQUAT IMU TIME
+for i in range(len(magdwick_acc)):
+    if(magdwick_acc[i] >= 12): # arbitrary threshold
+        print("Squat at: ", magdwick_times[i], " seconds")
+        break
+
+# Video setup
 cap = cv2.VideoCapture(video_path)
 fps = cap.get(cv2.CAP_PROP_FPS)
 
-FIRST_SQUAT_IMU_TIME = 5.8
-FIRST_SQUAT_VIDEO_TIME = 5.2
-VIDEO_IMU_DIFF = FIRST_SQUAT_IMU_TIME - FIRST_SQUAT_VIDEO_TIME
 
 # Initialize the plot could be extended to more axis?
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8))
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10, 8))
 ax1.set_title('Video Frame')
 ax1.axis('off')
-ax2.set_title('IMU Data (y-axis)')
+
+ax2.set_title('IMU Data Raw (y-axis)')
 ax2.set_xlabel('Time (s)')
-ax2.set_ylabel('Acceleration')
-line, = ax2.plot([], [], 'b-')
+ax2.set_ylabel('Acceleration (m/s²)')
+line1, = ax2.plot([], [], 'b-')
 ax2.set_xlim(imu_times.min(), imu_times.max())
 ax2.set_ylim(imu_y.min(), imu_y.max())
+
+ax3.set_title('IMU Data Magdwick')
+ax3.set_xlabel('Time (s)')
+ax3.set_ylabel('Acceleration (m/s²)')
+line2, = ax3.plot([], [], 'b-')
+ax3.set_xlim(magdwick_times.min(), magdwick_times.max())
+ax3.set_ylim(magdwick_acc.min(), magdwick_acc.max())
 
 # Initialize the image for the video frame
 img = ax1.imshow(np.zeros((480, 640, 3), dtype=np.uint8))
@@ -57,8 +80,9 @@ def update(frame_idx):
             current_frame +=1
 
     # Update the IMU plot
-    line.set_data(imu_times[:frame_idx+1], imu_y[:frame_idx+1])
-    return img, line
+    line1.set_data(imu_times[:frame_idx+1], imu_y[:frame_idx+1])
+    line2.set_data(magdwick_times[:frame_idx+1], magdwick_acc[:frame_idx+1])
+    return img, line1, line2
 
 # Create the animation
 ani = FuncAnimation(fig, update, frames=int(cap.get(cv2.CAP_PROP_FRAME_COUNT)),
